b5620b4ad510ec24c9bfc93c917bba0ebf0ca2ce
deshantar99 <deshantar.parajuli99@gmail.com>
2020-05-01 23:25:38 -0500

    updated nb,svm,knn,roccio;cleaned code

--- reddit.py
+++ reddit.py
@@ -3,6 +3,8 @@ import pandas as pd
 from praw.models import MoreComments
 import re
 
+reddit = praw.Reddit(client_id='ENTER CLIENT ID', client_secret='ENTER SECRET KEY', user_agent='ENTER USER AGENT')
+
 
 arr = ['JoeRogan', 'learnprogramming', 'chemistry', 'NoStupidQuestions', 'Tinder', 'history', 'legaladvice', 'learnjava', 'Parenting', 'Home', 'technology', 'funny', 'Art', 'india', 'LifeProTips', 'drawing', 'ama', 'techsupport', 'google', 'UTAustin', 'GetEmployed', 'dating_advice', 'computerscience', 'ethereum', 'Coronavirus', 'coding', 'CryptoCurrency', 'datascience', 'shrooms', 'explainlikeimfive', 'Fantasy', 'americanairlines', 'gaming', 'aww', 'gardening', 'aggies', 'AnimalCrossing', 'nasa', 'walmart', 'linux', 'travel', 'AskMen', 'Cooking', 'hiphopheads', 'videos', 'AskReddit', 'TAMU', 'btc', 'programming', 'texas', 'nfl', 'australia', 'iphone', 'memes', 'sports', 'philosophy', 'science', 'italy', 'ComputerEngineering', 'amazon', 'DIY', 'worldpolitics', 'atheism', 'MachineLearning', 'computer', 'help', 'hacking', 'worldnews', 'resumes', 'GetMotivated', 'mildlyinteresting', 'Music', 'JobFair', 'MovieDetails', 'biology', 'nba', 'manga', 'AskHistorians', 'nvidia', 'self', 'pharmacy', 'Showerthoughts', 'Economics', 'europe', 'lifehacks', 'keto', 'careerguidance', 'soccer', 'storage', 'marvelstudios', 'javahelp', 'cars', 'hiking', 'sysadmin', 'Bitcoin', 'trashy', 'financialindependence', 'AskWomen', 'UpliftingNews', 'Steam', 'csMajors', 'apple', 'CasualConversation', 'wallstreetbets', 'oculus', 'socialskills', 'gmaing', 'relationship_advice', 'movies', 'space', 'literature', 'tifu', 'camping', 'Minecraft', 'relationships', 'learnpython', 'wow', 'recipes', 'computervision', 'anime', 'snakes', 'conspiracy', 'news', 'cscareerquestions', 'modernwarfare', 'todayilearned', 'Astronomy', 'Jokes', 'exit', 'Futurology', 'teenagers', 'MMA', 'Python', 'food', 'guns', 'BasicIncome', 'unpopularopinion', 'dataisbeautiful', 'Documentaries', 'Fitness', 'jobs', 'pcgaming', 'askscience', 'datastorage', 'pics', 'bodybuilding', 'politics', 'offmychest', 'leagueoflegends']
 
@@ -18,5 +20,4 @@ for sub in arr:
 		title = re.sub(r'[" "]+', " ", title)
 		posts.append([title.strip(), post.subreddit.display_name.strip()])
 		f.writelines((str(posts[0]), "\n"))
-
 	f.close()
--- trained_model.py
+++ trained_model.py
@@ -9,6 +9,7 @@ from collections import defaultdict
 from nltk.corpus import wordnet as wn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import model_selection, metrics, naive_bayes, svm
+from sklearn.neighbors import KNeighborsClassifier, NearestCentroid
 import pickle
 
 test_data_file = 'split_data/test.csv'
@@ -18,6 +19,9 @@ train_file = pd.read_csv(train_data_file)
 test_file = pd.read_csv(test_data_file)
 
 def tokenize_string(wordStr):
+    #tokenizes all of the post titles in the wordStr array
+    #input: post['title'] for a subreddit data file
+    #output: tokenized post['title'] for a subreddit data file
     wordStr.dropna(inplace=True)
     wordStr = [entry.lower() for entry in wordStr]
     wordStr = [word_tokenize(entry) for entry in wordStr]
@@ -25,6 +29,14 @@ def tokenize_string(wordStr):
 
 
 def lemmatize_string(wordStr, fileptr, has_file_flag):
+    #lemmatizes all of the words in post titles
+    #input: wordStr: array of post['title'], fileptr: pointer to the datafile,
+    #input: has_file_flag: flag to decide whether to edit the file or return (cont.)
+    #input: an array of changes
+
+    #output: append lemmatized string to 'title_final' column of file if has_file_flag is 1
+    #output: return lemmatized string if has_file_flag is 0, needed when predict user queries
+
     tag_map = defaultdict(lambda : wn.NOUN)
     tag_map['J'] = wn.ADJ
     tag_map['V'] = wn.VERB
@@ -45,118 +57,121 @@ def lemmatize_string(wordStr, fileptr, has_file_flag):
             finalStr.append(str(Final_words))
             return finalStr
 
+
 def train_model(model_type):
+    #trains the model on the train data; model defined by model_type
+    #model_type: nb, svm, knn, rocchio
+
+    #tokenize train and test titles
     train_file['title'] = tokenize_string(train_file['title'])
     test_file['title'] = tokenize_string(test_file['title'])
 
+    #lemmatize train and test titles
     lemmatize_string(train_file['title'], train_file, 1)
     lemmatize_string(test_file['title'], test_file, 1)
 
+    #initialize train and test data
     Train_X, Test_X = train_file['title_final'], test_file['title_final']
     Train_Y, Test_Y = train_file['subreddit'], test_file['subreddit']
 
+    #Encode the labels to a number
     Encoder = LabelEncoder()
     Test_Y = Encoder.fit_transform(Test_Y)
     Train_Y = Encoder.fit_transform(Train_Y)
 
-    Tfidf_vect = TfidfVectorizer(max_features=5000)
+    #term frequency-inverse doc frequency
+    Tfidf_vect = TfidfVectorizer(max_features=5000) #assuming max # of words = 5000
     Tfidf_vect.fit(train_file['title_final'].append(test_file['title_final']))
     Train_X_Tfidf = Tfidf_vect.transform(Train_X)
     Test_X_Tfidf = Tfidf_vect.transform(Test_X)
 
-    if(model_type == 'nb'):
-        Naive = naive_bayes.MultinomialNB()
-        Naive.fit(Train_X_Tfidf, Train_Y)
+    #save trained utility values for later use
+    with open('local/utils.txt', 'wb') as utils_file:
+        pickle.dump(Tfidf_vect, utils_file)
+        pickle.dump(Train_X_Tfidf, utils_file)
+        pickle.dump(Test_X_Tfidf, utils_file)
 
-        with open('trained_nb.txt', 'wb') as NB_file:
-            pickle.dump(Naive, NB_file)
-            pickle.dump(Tfidf_vect, NB_file)
-            pickle.dump(Train_X_Tfidf, NB_file)
-            pickle.dump(Train_Y, NB_file)
+    if(model_type == 'nb'):
+        NB = naive_bayes.MultinomialNB()
+        NB.fit(Train_X_Tfidf, Train_Y)
+        train_NB = NB.predict(Train_X_Tfidf)
+        print('Naive Bayes Train Accuracy: ', metrics.accuracy_score(train_NB, Train_Y) * 100)
+        test_NB = NB.predict(Test_X_Tfidf)
+        print('Naive Bayes Test Accuracy: ', metrics.accuracy_score(test_NB, Test_Y) * 100)
+
+        with open('local/trained_nb.txt', 'wb') as NB_file:
+            pickle.dump(NB, NB_file)
 
     elif(model_type == 'svm'):
         SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
         SVM.fit(Train_X_Tfidf, Train_Y)
 
-        predict_SVM = SVM.predict(Test_X_Tfidf)
         train_svm = SVM.predict(Train_X_Tfidf)
-        print('SVM train score: ' + str(accuracy_score(train_svm, Train_Y) * 100))
-        print("SVM accuracy_score: " + str(accuracy_score(predict_SVM, Test_Y) * 100))
+        print('SVM Train Accuracy: ' + str(metrics.accuracy_score(train_svm, Train_Y) * 100))
+        test_SVM = SVM.predict(Test_X_Tfidf)
+        print("SVM Test score: " + str(metrics.accuracy_score(test_SVM, Test_Y) * 100))
+
+        with open('trained_svm.txt', 'wb') as SVM_file:
+            pickle.dump(SVM, SVM_file)
+
+    elif(model_type == 'knn'):
+        KNN = KNeighborsClassifier(n_neighbors = 3)
+        KNN.fit(Train_X_Tfidf, Train_Y)
+        train_KNN = KNN.predict(Train_X_Tfidf)
+        print('KNN Train Accuracy: ' + str(metrics.accuracy_score(train_KNN, Train_Y) * 100))
+        test_KNN = KNN.predict(Test_X_Tfidf)
+        print('KNN Test Accuracy: ' + str(metrics.accuracy_score(test_KNN, Test_Y) * 100))
+
+        with open('local/trained_knn.txt', 'wb') as KNN_file:
+            pickle.dump(KNN, KNN_file)
+
+    elif(model_type == 'rocchio'):
+        Rocchio = NearestCentroid()
+        Rocchio.fit(Train_X_Tfidf, Train_Y)
+        print('fitted')
+        train_Rocchio = Rocchio.predict(Train_X_Tfidf)
+        print('Rocchio Train Accuracy: ' + str(metrics.accuracy_score(train_Rocchio, Train_Y) * 100))
+        test_Rocchio = Rocchio.predict(Test_X_Tfidf)
+        print('Rocchio Test Accuracy: ' + str(metrics.accuracy_score(test_Rocchio, Test_Y) * 100))
+
+        with open('local/trained_rocchio.txt', 'wb') as Rocchio_file:
+            pickle.dump(Rocchio, Rocchio_file)
 
 
+train_choice = input('Do you want to train again? (y/n): (TAKES A LONG TIME)')
 
-
-train_choice = input('Do you want to train again? (y/n)')
-
+#takes a very long time, train each model individually
+#train one, comment the rest
+#each time a model is trained again, it is locally saved
+#
 if(train_choice == 'y'):
+    train_model('nb')
+    train_model('svm')
+    train_model('knn')
+    train_model('rocchio')
 
-    # train_file['title'] = tokenize_string(train_file['title'])
-    # test_file['title'] = tokenize_string(test_file['title'])
-    #
-    # print('----finished tokenizing------')
-    #
-    # lemmatize_string(train_file['title'], train_file, 1)
-    # lemmatize_string(test_file['title'], test_file, 1)
-    #
-    # print('----finished lemmatizing-------')
-    #
-    # Train_X, Test_X = train_file['title_final'], test_file['title_final']
-    # Train_Y, Test_Y = train_file['subreddit'], test_file['subreddit']
-    #
-    # print('-----finished setting data--------')
-    #
-    #
-    # Encoder = LabelEncoder()
-    # Test_Y = Encoder.fit_transform(Test_Y)
-    # Train_Y = Encoder.fit_transform(Train_Y)
-    #
-    # Tfidf_vect = TfidfVectorizer(max_features=5000)
-    # Tfidf_vect.fit(train_file['title_final'].append(test_file['title_final']))
-    # Train_X_Tfidf = Tfidf_vect.transform(Train_X)
-    # Test_X_Tfidf = Tfidf_vect.transform(Test_X)
-    #
-    #
-    # print('---finished fitting-----')
-    #
-    # Naive = naive_bayes.MultinomialNB()
-    # Naive.fit(Train_X_Tfidf, Train_Y)
-    #
-    # with open('trained_nb.txt', 'wb') as NB_file:
-    #     pickle.dump(Naive, NB_file)
-    #     pickle.dump(Tfidf_vect, NB_file)
-    #     pickle.dump(Train_X_Tfidf, NB_file)
-    #     pickle.dump(Train_Y, NB_file)
-    pass
 
-with open('utils.txt', 'rb') as utils_file:
+with open('local/utils.txt', 'rb') as utils_file:
     Tfidf_vect = pickle.load(utils_file)
     Train_X_Tfidf = pickle.load(utils_file)
+    Test_X_Tfidf = pickle.load(utils_file)
 
-with open('trained_nb.txt', 'rb') as NB_file:
-    Naive = pickle.load(NB_file)
-    # Tfidf_vect = pickle.load(NB_file)
-    # Train_X_Tfidf = pickle.load(NB_file)
-    # Train_Y = pickle.load(NB_file)
+with open('local/trained_nb.txt', 'rb') as NB_file:
+    NB = pickle.load(NB_file)
+
+with open('local/trained_svm.txt', 'rb') as SVM_file:
+    SVM = pickle.load(SVM_file)
+
+with open('local/trained_knn.txt', 'rb') as KNN_file:
+    KNN = pickle.load(KNN_file)
+
+with open('local/trained_rocchio.txt', 'rb') as Rocchio_file:
+    Rocchio = pickle.load(Rocchio_file)
 
 
 Encoder = LabelEncoder()
 Train_Y = train_file['subreddit']
 Train_Y = Encoder.fit_transform(Train_Y)
-
-# train_NB = Naive.predict(Train_X_Tfidf)
-# # test_NB = Naive.predict(Test_X_Tfidf)
-# #
-# print('-'*5 + 'TRAIN' + '-'*5)
-# print('Accuracy: ', metrics.accuracy_score(train_NB, Train_Y) * 100)
-# print('Precision: ', metrics.precision_score(train_NB, Train_Y, average='micro') * 100)
-# print('Recall: ', metrics.recall_score(train_NB, Train_Y, average='micro') * 100)
-# print('F1: ', metrics.f1_score(train_NB, Train_Y, average='micro') * 100)
-# #
-# print('-'*5 + 'TEST' + '-'*5)
-# print('Accuracy: ', metrics.accuracy_score(test_NB, Test_Y) * 100)
-# print('Precision: ', metrics.precision_score(test_NB, Test_Y, average='micro') * 100)
-# print('Recall: ', metrics.recall_score(test_NB, Test_Y, average='micro') * 100)
-# print('F1: ', metrics.f1_score(test_NB, Test_Y, average='micro') * 100)
 
 query = ''
 while(query != 'exit'):
@@ -166,5 +181,12 @@ while(query != 'exit'):
     query = lemmatize_string(query, None, 0)
 
     query_Tfidf = Tfidf_vect.transform(query)
-    predicted = Naive.predict(query_Tfidf)
-    print(Encoder.inverse_transform(predicted))
+    Rocchio_predict = Rocchio.predict(query_Tfidf)
+    KNN_predict = KNN.predict(query_Tfidf)
+    SVM_predict = SVM.predict(query_Tfidf)
+    NB_predict = NB.predict(query_Tfidf)
+
+    print('NB: ' + str(Encoder.inverse_transform(NB_predict)))
+    print('SVM: ' + str(Encoder.inverse_transform(SVM_predict)))
+    print('KNN: ' + str(Encoder.inverse_transform(KNN_predict)))
+    print('Rocchio: ' + str(Encoder.inverse_transform(Rocchio_predict)))
